f290c3b98c8a4cbf51653d41cf40cb9a4bdb69da
JerrysIRL <sergei.maltcev@edu.futuregames.nu>
2024-12-10 20:08:41 +0100

    Day3 PartOne

---
+++ Day3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+
+string input = File.ReadAllText("E:\\AdventOfCode\\AdventOfCode2024\\Day3\\input.txt");
+MatchCollection matches =  Regex.Matches(input,"mul\\(\\d{1,3},\\d{1,3}\\)");
+
+int total = 0;
+foreach (Match match in matches)
+{
+    var nums = Regex.Match(match.Value, "\\d{1,3},\\d{1,3}");
+    var numsArr = nums.Value.Split(",").Select(int.Parse);
+    var result = numsArr.Aggregate((a,b) => a * b);
+    total += result;
+}
+
+Console.WriteLine(total);
